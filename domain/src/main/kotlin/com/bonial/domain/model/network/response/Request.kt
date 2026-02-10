43a24e05fd2451d49eb133527d0539c21569ebd3
Abdul Samad <samad.qurashi85@gmail.com>
2026-02-10 17:37:17 +0500

    api integration inprogress

--- domain/src/main/kotlin/com/bonial/domain/model/network/response/Request.kt
+++ domain/src/main/kotlin/com/bonial/domain/model/network/response/Request.kt
@@ -1,7 +1,19 @@
 package com.bonial.domain.model.network.response
 
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
 sealed class Request<out T> {
     object Loading : Request<Nothing>()
     data class Error(val apiError: ApiError?) : Request<Nothing>()
     data class Success<out T>(val data: T) : Request<T>()
 }
+
+@Parcelize
+data class ApiError(
+    @SerializedName("code")
+    val code: String,
+    @SerializedName("message")
+    val message: String,
+): Parcelable
